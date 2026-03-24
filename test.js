da51f0ba222ce94f8b5f3cccb9e41b014f70b815
amine sakhri <sakhriamine2003@gmail.com>
2026-03-24 17:50:26 +0100

    challenge 2

--- test.js
+++ test.js
@@ -4,54 +4,95 @@ const bttn1 = document.getElementById("bttn1");
 const Input = document.getElementById("Input");
 
 
-btn.onclick = () => {
-  if (body.style.backgroundColor === "black") {
-    body.style.backgroundColor = "white";
+// btn.onclick = () => {
+//   if (body.style.backgroundColor === "black") {
+//     body.style.backgroundColor = "white";
+//   } else {
+//     body.style.backgroundColor = "black";
+//   }
+// };
+
+// bttn1.onclick = () => {
+//   Input.value = Number(Input.value) + 1;
+// };
+
+// bttn2.onclick = () => {
+//   Input.value = Number(Input.value) - 1;
+// };
+
+// bttn3.onclick = () => {
+//   Input.value = 0;
+// };
+
+
+
+// const input = document.getElementById("input");
+// const paragraphe = document.getElementById("p");
+
+// input.oninput = () => {
+//     paragraphe.textContent = input.value;
+// };
+
+
+// const changer = document.getElementById("changer");
+// const pp = document.getElementById("pp");
+
+// const table = [
+//     "javascripts",
+//     "java",
+//     "ttttttttt",
+//     "ffffffffff",
+//     "yyyyyyyyyyy",
+//     "azerty2003"
+// ];
+
+// let clik = 0; 
+// changer.onclick = () => {
+//     pp.textContent += table[clik] + "\ /"; 
+//     clik++;
+
+//     if (clik >= table.length) {
+//         clik = 0;
+//     }
+// };
+// challenge 2  => Challenges de Logique & Formulaires: 
+
+const nom =document.getElementById("nom");
+const prenom =document.getElementById("prenom");
+const code =document.getElementById("code");
+const suivant =document.getElementById("suivant");
+const precident =document.getElementById("a");
+const message =document.getElementById("msg")
+
+suivant.onclick = () => {
+  console.log("botona");
+
+  if (
+    code.value.length < 8 ||
+    !/[A-Z]/.test(code.value) ||
+    !/[0-9]/.test(code.value)
+  ) {
+    message.textContent = "Mot de passe invalide";
+
   } else {
-    body.style.backgroundColor = "black";
+    message.textContent = "Valide ✅";
   }
 };
 
-bttn1.onclick = () => {
-  Input.value = Number(Input.value) + 1;
-};
 
-bttn2.onclick = () => {
-  Input.value = Number(Input.value) - 1;
-};
+const desc = document.getElementById("desc");
 
-bttn3.onclick = () => {
-  Input.value = 0;
-};
-
+desc.addEventListener("input", () => {
+  if (desc.value.length >= 280) {
+    desc.style.color = "red";
+  } else {
+    desc.style.color = "";
+  }
+});
 
 
-const input = document.getElementById("input");
-const paragraphe = document.getElementById("p");
-
-input.oninput = () => {
-    paragraphe.textContent = input.value;
-};
 
 
-const changer = document.getElementById("changer");
-const pp = document.getElementById("pp");
 
-const table = [
-    "javascripts",
-    "java",
-    "ttttttttt",
-    "ffffffffff",
-    "yyyyyyyyyyy",
-    "azerty2003"
-];
 
-let clik = 0; 
-changer.onclick = () => {
-    pp.textContent += table[clik] + "\ /"; 
-    clik++;
 
-    if (clik >= table.length) {
-        clik = 0;
-    }
-};
